8029786317058caddcfa4eb7710dc779f6ae7af3
oussama-futureroc <110825282+oussama-futureroc@users.noreply.github.com>
2022-08-11 14:05:04 +0100

    Update create-network-security-rule.sh

--- create-network-security-rule.sh
+++ create-network-security-rule.sh
@@ -6,3 +6,10 @@ az network nsg rule create \
   --priority 100 \
   --destination-port-range 80 \
   --access Allow
+
+#show simple to verify
+az network nsg rule list \
+  --resource-group learn-71c830d2-e715-4c6b-bd53-036a8f646efd \
+  --nsg-name my-vmNSG \
+  --query '[].{Name:name, Priority:priority, Port:destinationPortRange, Access:access}' \
+  --output table
