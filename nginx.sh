65dfc8dbf878082b8f83522138448204965a896f
oussama-futureroc <110825282+oussama-futureroc@users.noreply.github.com>
2022-08-11 13:43:29 +0100

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,8 @@
+az vm extension set \
+  --resource-group learn-71c830d2-e715-4c6b-bd53-036a8f646efd \
+  --vm-name my-vm \
+  --name customScript \
+  --publisher Microsoft.Azure.Extensions \
+  --version 2.1 \
+  --settings '{"fileUris":["https://raw.githubusercontent.com/MicrosoftDocs/mslearn-welcome-to-azure/master/configure-nginx.sh"]}' \
+  --protected-settings '{"commandToExecute": "./configure-nginx.sh"}'
