5ffcbb94b5bbca702beceecf6de40653c64bb47c
oussama-futureroc <110825282+oussama-futureroc@users.noreply.github.com>
2022-08-11 14:02:49 +0100

    Update vm-security-rules.sh

--- vm-security-rules.sh
+++ vm-security-rules.sh
@@ -1,8 +1,18 @@
+#security group
 az network nsg list \
   --resource-group learn-71c830d2-e715-4c6b-bd53-036a8f646efd \
   --query '[].name' \
   --output tsv
-  
-  az network nsg rule list \
+
+#rules
+az network nsg rule list \
   --resource-group learn-71c830d2-e715-4c6b-bd53-036a8f646efd \
   --nsg-name my-vmNSG
+  
+  
+#rules in table
+az network nsg rule list \
+  --resource-group learn-71c830d2-e715-4c6b-bd53-036a8f646efd \
+  --nsg-name my-vmNSG \
+  --query '[].{Name:name, Priority:priority, Port:destinationPortRange, Access:access}' \
+  --output table
